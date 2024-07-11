8732124460f4b9a361632f6f37e478c608fe511a
Arden Feldt <feldt@ad.unc.edu>
2024-07-11 14:28:07 -0400

    structure of site

---
+++ includes/footer.php
@@ -0,0 +1,13 @@
+<html>
+<body>
+<div>
+<div class="footer">
+    <p>&copy; <?php echo date("Y"); ?> Classic Scheduling. All rights reserved.</p>
+    <div class="footer">
+        <p>&copy; <?php echo date("Y"); ?> Classic Scheduling. All rights reserved.</p>
+        <img src="assets/img/statistics_logo_white-1.png" alt="STOR logo" width="1000">
+    </div>
+
+</div>
+</body>
+</html>
